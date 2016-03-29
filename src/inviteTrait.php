7f37a92d05f3796c9e3a8e6e5e5a0c6c905aa540
Muhammad Junaid Nasir <junaidnasir@live.com>
2016-03-29 21:15:26 +0500

    added trait

---
+++ src/inviteTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Junaidnasir\Larainvite;
+
+trait InviteTrait {
+    /**
+     * return all invitation as laravel collection
+     * @return hasMany invitation Models
+     */
+    public function invitations()
+    {
+        return $this->hasMany('Junaidnasir\Larainvite\Models\LaraInviteModel');
+    }
+
+    /**
+     * return successful initation by a user
+     * @return hasMany
+     */
+    public function invitationSuccess()
+    {
+        return $this->invitations()->where('status','successful');
+    }
+    /**
+     * return pending invitations by a user
+     * @return hasMany
+     */
+    public function invitationPending()
+    {
+        return $this->invitations()->where('status','pending');
+    }
+}
